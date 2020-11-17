cffe1f51eadc73b9884fcdd267bece029e96662d
Tabatha Macedo <33585625+tabskey@users.noreply.github.com>
2020-11-17 11:22:57 -0300

    Fixed the read, finally!!!

--- src/App.vue
+++ src/App.vue
@@ -87,15 +87,15 @@ export default {
         var loadingTask = pdfjsLib.getDocument({ data: pdfData });
         // this.accessPDF();
         console.log(loadingTask);
-        loadingTask.promise.then(function(pdf) {
+        loadingTask.promise.then((pdf) =>{
           var pdfDocument = pdf;
           var pagesPromises = [];
 
           for (var i = 0; i < pdf.numPages; i++) {
             // Required to prevent that i is always the total of pages
-            (function(pageNumber) {
-              pagesPromises.push(this.getPageText(pageNumber, pdfDocument));
-            })(i + 1);
+            //(function(pageNumber) {
+              pagesPromises.push(this.getPageText(i + 1, pdfDocument));
+           // })(i + 1);
           }
 
           Promise.all(pagesPromises).then(function(pagesText) {
@@ -105,78 +105,8 @@ export default {
         });
 
         }
-
-
-
-        // var fileToLoad = files[0];
-        // var fileReader = new FileReader();
-        // var base64File;
-        // // Reading file content when it's loaded
-        // fileReader.onload = function(event) {
-        //     base64File = event.target.result;
-        //     // base64File console
-        //     console.log(base64File);
-        //     var urlBase64 = base64File.replace(/^data:.+;base64,/, "");
-        
-
-        // // Convert data to base64
-        // var teste = fileReader.readAsDataURL(fileToLoad);
-        //  var pdfData = atob(urlBase64);
-        // var pdfjsLib = window["pdfjs-dist/build/pdf"];
-        // pdfjsLib.GlobalWorkerOptions.workerSrc = '//mozilla.github.io/pdf.js/build/pdf.worker.js';
-        // var loadingTask = pdfjsLib.getDocument({ data: pdfData });
-      
-        // // this.accessPDF();
-        // console.log(loadingTask);
-        // this.loadingTask.promise.then(function(pdf) {
-        //   var pdfDocument = pdf;
-        //   var pagesPromises = [];
-
-        //   for (var i = 0; i < pdf.numPages; i++) {
-        //     // Required to prevent that i is always the total of pages
-        //     (function(pageNumber) {
-        //       pagesPromises.push(this.getPageText(pageNumber, pdfDocument));
-        //     })(i + 1);
-        //   }
-
-        //   Promise.all(pagesPromises).then(function(pagesText) {
-        //     // Display text of all the pages in the console
-        //     console.log(pagesText);
-        //   });
-        // });
-        //   };
         }
      },
-    // convertToURL() {
-    //   const objFile = this.$refs["file_id"];
-    //   if (objFile.value === "") {
-    //     alert("Please select a File！"); // inserir um modal futuramente
-    //     return;
-    //   } else {
-    //     const files = objFile.state.uploadFiles;
-    //     console.log("Files");
-    //     console.log(files);
-    //     if (files.length > 0) {
-    //       var fileToLoad = files[0].raw;
-    //       //var fileReader = new FileReader();
-    //     }
-    //     return fileToLoad;
-    //   }
-    // },
-    // previewPDF() {
-    //   var files = document.querySelector("input[type=file]").files;
-    //   console.log(files);
-    //   if (files && files.length >= 1) {
-    //     files.foreach(file => {
-    //       this.convertToURL(file)
-    //         .then(data => {
-    //           console.log(data);
-    //           console.log(file); // 把編碼後的字串輸出到console
-    //         })
-    //         .catch(error => console.log(error));
-    //     });
-    //   }
-    // },
     convertTobase64() {
       const objFile = this.$refs["file_id"];
       if (objFile.value === "") {
@@ -220,9 +150,9 @@ export default {
 
           for (var i = 0; i < pdf.numPages; i++) {
             // Required to prevent that i is always the total of pages
-            (function(pageNumber) {
-              pagesPromises.push(this.getPageText(pageNumber, pdfDocument));
-            })(i + 1);
+            //(function() { //(PageNumber)
+              pagesPromises.push(this.getPageText(i + 1, pdfDocument));
+           // });
           }
 
           Promise.all(pagesPromises).then(function(pagesText) {
@@ -248,9 +178,9 @@ export default {
             }
 
             // Solve promise with the text retrieven from the page
+           
+            console.log(finalString); 
             resolve(finalString);
-            console.log(finalString);
-            return finalString;
           });
         });
       });
